dd1f7faf229cb992f7b4c413b25c49f56167ac17
Justin <jreaylayton@gmail.com>
2021-06-11 15:04:05 -0700

    Grid now colors and clears correctly.

--- eas.js
+++ eas.js
@@ -1,15 +1,34 @@
 // Create 16x16 grid of square divs
-const container = document.getElementById("container");
-
 function makeGrid(size) {
+    const container = document.getElementById("container");
     for (i = 0; i < size; i++) {
         for (j = 0; j < size; j++) {
             let cell = document.createElement("div");
             cell.className = "grid-item";
+            cell.id = "grid-item" + i + j;
             container.appendChild(cell);
         }
     }
 }
 
+function sketch() {}
+
+function colorCell(e) {
+    e.target.style.backgroundColor = "red";
+}
+
+function clearGrid() {
+    let elements = document.getElementsByClassName("grid-item");
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "black";
+    }
+}
 
 makeGrid(16);
+
+const cells = document.querySelectorAll(".grid-item");
+
+cells.forEach(div => div.addEventListener("mouseover", colorCell));
+
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", clearGrid);
